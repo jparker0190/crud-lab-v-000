494abe38ee2652e18179e62f00d36b6c94540007
Joseph Parker <jparker010190@gmail.com>
2018-09-24 00:37:41 +0000

    automatically backed up by learn

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
-import ReviewsContainer from '../../containers/ReviewsContainer'
+import ReviewInput from '../reviews/ReviewInput'
 
 class Restaurant extends Component {
-
+  handleOnClick() {
+    this.props.store.dispatch({
+      type: 'DELETE_RESTAURANT',
+      id: this.props.restaurant.id
+    });
+  }
 
   render() {
-    const { restaurant } = this.props;
-
     return (
       <div>
-        <li>
-          {restaurant.text}
-          <button> X </button>
-          <ReviewsContainer restaurant={restaurant}/>
-        </li>
+        <div>
+          <li>{this.props.restaurant.text}</li> 
+          <button onClick={() => this.handleOnClick()} >
+            Delete 
+          </button>
+        </div>
+        <ReviewInput store={this.props.store} 
+                    restaurantId={this.props.restaurant.id}/>
       </div>
     );
   }
